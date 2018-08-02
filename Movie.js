757ec2f99228393e636fb2a4d54d42f71cb9b7bb
Brennan Ausloos <bma16b@my.fsu.edu>
2018-08-02 17:49:27 +0000

    Lesson 8 final commit

--- Movie.js
+++ Movie.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
 
             $.ajax({
                 //Insert YOUR API Key after the '=' inside the single quotes.
-                url:'https://api.themoviedb.org/3/search/movie?api_key=INSERT-HERE',
+                url:'https://api.themoviedb.org/3/search/movie?api_key=1ebc1f26812574306ef989564930bd73',
                 data: query
             })
             
@@ -57,6 +57,7 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/                
                 
+                $("#overview").html(json.results[0].overview);
                 
                 
 
@@ -71,7 +72,8 @@ $(document).ready(function(){
                  3.  Use the example above where the Movie Title was retrieved to complete this step!
                  
                  *********************************Insert Code Below********************************************/               
-
+                
+                $("#release_date").html(json.results[0].release_date);
 
 
 
@@ -87,7 +89,7 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/   
 
-  
+                $("#popularity").html(json.results[0].popularity);
 
                 
                  /*****************Retrieve Movie Original Language from Server & Add to HTML Document***********
@@ -102,6 +104,7 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/                 
 
+                $("#original_language").html(json.results[0].original_language);
 
 
                 //The following code retrieves the movie poster path from the JSON object retrieved from the TMDB 
--- README.md
+++ README.md
@@ -4,19 +4,18 @@
 LIS 2360:  Web Application Development
 
 ### Assignment Date:  
-(Month Day, Year)
+August 2, 2018
 
 ### Student Name:  
-(First and Last Name)
+Brennan Ausloos
 
 ### Project Description:
-(In your own words, briefly describe the assignment.)
+In this project I was tasked with using TMDB's API in my java code to display six different features of any movie title entered into the program.
 
 ### View Project:
-(Replace this statement with your Github Page URL that was created when you 
- published the project.)
+https://github.com/brennanausloos/lesson8_ajax
 
 ### Lessons Learned in the Assignment:
-1. (Briefly describe a lesson/concept learned in this lesson.)
-2. (Briefly describe a lesson/concept learned in this lesson.)
-3. (Briefly describe a lesson/concept learned in this lesson.)
+1. In this lesson I learned how to request an API from TMDB website.
+2. In this lesson I learned how to use jQUery code to retrieve diffeent movie elements from the JSON object.
+3. In this lesson I learned how to insert my API key into javascript to retrieve data from TMDB.
